04c928026280c2f5db9c21639bc405baa466f6bd
chicagoist <chicagoist@exploit.im>
2024-01-28 19:40:50 +0100

    После досмотра урока

--- 09. Stream API/src/lesson09/task1/classwork/Main.java
+++ 09. Stream API/src/lesson09/task1/classwork/Main.java
@@ -1,5 +1,9 @@
 package lesson09.task1.classwork;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Director worker1 = new Director();
@@ -33,6 +37,36 @@ public class Main {
         System.out.println(result);
         System.out.println(result1);
 
+        // Фильтр с Predicate
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            numbers.add((int) (Math.random() * 1000));
+        }
 
+        Predicate predicate = (n) -> {
+            return n % 2 == 0;
+        };
+
+        // Разные варианты
+        //   List<Integer> filtered = filter(numbers,predicate);
+          List<Integer> filtered = filter(numbers,(n) -> n % 2 == 0);
+/*        List<Integer> filtered = filter(numbers,(n) -> {
+            return n % 3 == 0;
+        });
+*/
+        System.out.println("---FILTERED---");
+        System.out.println(filtered);
+    }
+
+
+    static List<Integer> filter(List<Integer> numbers, Predicate obj) {
+        List<Integer> result = new ArrayList<>();
+        for(int i : numbers) {
+            if(obj.test(i)) {
+                result.add(i);
+            }
+        }
+        Collections.sort(result);
+        return result;
     }
 }
---
+++ 09. Stream API/src/lesson09/task1/classwork/Predicate.java
@@ -0,0 +1,6 @@
+package lesson09.task1.classwork;
+
+@FunctionalInterface
+public interface Predicate {
+    boolean test(int a);
+}
